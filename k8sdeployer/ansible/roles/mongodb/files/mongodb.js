5ac9c21676c9668614e6d4903d7f7bbf68ee0ae7
ssingla <ssingla@redhat.com>
2026-01-21 11:48:18 +0530

    fixed some apps

--- k8sdeployer/ansible/roles/mongodb/files/mongodb.js
+++ k8sdeployer/ansible/roles/mongodb/files/mongodb.js
@@ -1,6 +1,6 @@
-db.createCollection("test_db", { size: 104857600 } )
+db.createCollection("test_db")
 doc={'a':0, 'item':9}
-db.test_db.save(doc)
+db.test_db.insertOne(doc)
 doc={'a':1, 'item':99}
-db.test_db.save(doc)
+db.test_db.insertOne(doc)
 db.test_db.find();
